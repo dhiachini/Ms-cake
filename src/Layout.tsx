f52aac2a1087e1b62553ae89a1b41dce860011e4
unknown <chinidhia@gmail.com>
2025-09-13 03:36:05 +0200

    landing page pc screens done

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { BookOpenText } from "lucide-react";
+import EbookIcon from "./assets/icons/EbookIcon";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -12,10 +14,47 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <div className="sticky top-0 w-full z-50">
         <Navbar />
       </div>
-      <div className="w-full">{children}</div>
-      <div className="w-full">
-        <Footer />
+      <div className="w-full h-screen">{children}</div>
+
+      <div className="flex w-full justify-center items-center p-8 bg-[#fffcf5]">
+        <div className="mt-40 w-full">
+          {/* Titre centré */}
+          <div className="flex justify-center">
+            <span className="text-5xl text-[#342520]">
+              Patisseries du week-end
+            </span>
+          </div>
+
+          {/* Les 3 boxes */}
+          <div className="flex justify-center gap-8 py-12 ">
+            <img
+              src="/src/assets/images/article-weekend-1.png"
+              alt="Photo 1"
+              className="object-cover w-70 h-78 "
+            />
+
+            <img
+              src="/src/assets/images/article-weekend-2.png"
+              alt="Photo 2"
+              className="object-cover w-70 h-78"
+            />
+            <img
+              src="/src/assets/images/article-weekend-3.png"
+              alt="Photo 3"
+              className="object-cover w-70 h-78"
+            />
+          </div>
+          {/* Nouveau div avec bg #fae5e4 */}
+          <div className="flex justify-center ">
+            <div className="flex w-[57rem] h-40 bg-[#fae5e4] flex-row justify-center items-center gap-10 p-5">
+              <EbookIcon className="w-40 h-40" />
+              <span className="text-4xl text-[#342520]">E-book Design Cake pour débutants</span>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,51 @@
+import { Cake, ChefHat, HandPlatter } from "lucide-react";
 import Layout from "../Layout";
 
 function HomePage() {
   return (
     <Layout>
-      <div className="space-y-7">
-        <div className="banner py-24">
+      <div className=" space-y-7">
+        <div className="banner py-24  h-screen ">
           <div className="container space-y-3">
-            <h1 className=" text-2xl max-w-[260px] ">
-              La haute pâtisserie française, personalisée pour vos plus belles
+            <h1 className="font-serif text-5xl text-[#342520] ">
+              La haute pâtisserie
+              <br /> française, personalisée <br /> pour vos plus belles
+              <br />
               occasions
             </h1>
-            <button className="bg-amber-100 text-black px-3 py-1 rounded-3xl cursor-pointer border-0 outline-none">
+            <button className="bg-[#faf4e6] hover:bg-[#b06c74] hover:text-[#faf4e6] text-black px-3 py-1 h-[50px] mt-9 w-[180px] rounded-3xl cursor-pointer border-0 outline-none">
               Découvrez-nous
             </button>
+          </div>
+          {/* Div ajouté après les images */}
+        </div>
+        {/* Box positionnées à 50% du bas de la bannière */}
+        <div className="absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[100%] w-full flex justify-center z-10">
+          <div className="flex flex-col md:flex-row gap-8 items-center">
+            <div className="bg-[#fdf5f2] w-70 h-56 p-1 flex flex-col justify-center items-center">
+              <Cake className="w-15 h-15 mb-2" />
+              <span className="text-3xl text-[#1d110f] text-center mb-3">
+                Je commande <br />
+              </span>
+              <span>Cake personnalisé</span>
+            </div>
+            <div className="bg-[#fdf5f2] w-70 h-56 p-1 flex flex-col justify-center items-center">
+              <HandPlatter className="w-15 h-15 mb-2" />
+              <span className="text-3xl text-[#1d110f] text-center mb-3">
+                Je découvre <br />
+              </span>
+              <span>Service traiteur</span>
+            </div>{" "}
+            <div className="bg-[#fdf5f2] w-70 h-56 p-1 flex flex-col justify-center items-center">
+              <ChefHat className="w-15 h-15 mb-2" />
+              <span className="text-3xl text-[#1d110f] text-center">
+                Je réserve <br />
+              </span>
+              <span className="text-xl text-[#1d110f] text-center">
+                Ateliers <br />
+              </span>
+              <span>pâtisserie & design cake</span>
+            </div>
           </div>
         </div>
       </div>
