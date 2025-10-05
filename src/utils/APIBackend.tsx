f6f1b1565d8e35c74af6ed4dac69f9c24dd928f0
unknown <chinidhia@gmail.com>
2025-10-05 20:12:20 +0200

    axios from dhia

---
+++ src/utils/APIBackend.tsx
@@ -0,0 +1,28 @@
+import axios, { type AxiosInstance } from "axios";
+
+
+const APIBackend: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "https://localhost:5050",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+APIBackend.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+APIBackend.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+);
+
+export default APIBackend;
